17a5521472511992128381a8020f92d0dd4ade17
benthebear93 <nswve@naver.com>
2020-02-04 10:17:54 +0900

    update README

--- README.md
+++ README.md
@@ -4,14 +4,19 @@ All the code in here just for the data crawling practice
 
 
 
-* save_img_all.py & save_img_week.py :: naver webtoon page crawling
+* #### save_img_all.py & save_img_week.py :: naver webtoon page crawling
 
-* address : https://comic.naver.com/webtoon/weekday.nhn
+  * address : https://comic.naver.com/webtoon/weekday.nhn
 
-  - save_img_all : save all the image(jpg, gif, png, etc)
+  * save_img_all.py : save all the image(jpg, gif, png, etc)
 
-  - save_img_week.py : save webtoon thumbnail by day of the week.
+  * save_img_week.py : save webtoon thumbnail by day of the week.
 
     it makes a folder for each day, and save the day's webtoon thumbnail
-    
+
     explain in Korean(한글 설명 : https://blog.naver.com/nswve/221775882686)
+
+* #### cafe_crawling :: cafe keyword search title save to csv
+
+  * address : https://m.cafe.naver.com/ArticleSearchList.nhn?search.query=%EA%B0%80%EB%A1%9C%EC%88%98%EA%B8%B8%EB%A7%9B%EC%A7%91&search.menuid=&search.searchBy=1&search.sortBy=date&search.clubid=11262350&search.option=0&search.defaultValue=1
+* cafe_crawling.py : save the specific keyword search result titles to csv
--- cafe_crawling.py
+++ cafe_crawling.py
@@ -1,51 +1,26 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup as bs
+import pandas as pd
 
-#crawling naver cafe data
-import dryscrape
-import sys
-import urllib.request
-from bs4 import BeautifulSoup
-import os
+driver = webdriver.Chrome()
+driver.implicitly_wait(3)
+base_url = " https://m.cafe.naver.com/ArticleSearchListAjax.nhn?search.query=%EA%B0%80%EB%A1%9C%EC%88%98%EA%B8%B8%EB%A7%9B%EC%A7%91&search.menuid=0&search.searchBy=0&search.sortBy=date&search.clubid=11262350&search.option=0&search.defaultValue=&search.page=1"
 
-def get_list():
-	#url = "https://m.cafe.naver.com/ArticleSearchList.nhn?search.query="+ keyword +"&search.searchBy=0&search.sortBy=date&search.clubid=11262350&search.option=0&search.defaultValue=&search.page=1" 
-	url = "https://m.cafe.naver.com/ArticleSearchListAjax.nhn?search.query=%EA%B0%80%EB%A1%9C%EC%88%98%EA%B8%B8%EB%A7%9B%EC%A7%91&search.menuid=&search.searchBy=1&search.sortBy=date&search.clubid=11262350&search.option=0&search.defaultValue=&search.page=2"
-	#str(page)
-	type(url)
-	req = urllib.request.Request(url)
-	html = urllib.request.urlopen(req).read()
-	soup = BeautifulSoup(html, 'html.parser')
-	return soup.select("a")
+driver.get(base_url)
 
-def get_link(dom) : 
-    ls = []
-    for i in range(0, len(dom)) :
-        link = dom[i].get('href')
-        if len(link) > 2 and "Comment" not in link and "javascript" not in link :
-            link = "http://m.cafe.naver.com"+link
-            ls.append(link)
-    return ls
+contentSource = driver.page_source
+#print(contentSource)
+soup = bs(contentSource, 'html.parser')
+#title = soup.find_all("div", {"class" : "tit"})
+title = soup.find_all('h3')
+print(len(title))
+title_list = []
+for tit_num in range(len(title)):
+	title2 = soup.select('h3')[tit_num].get_text()
+	title_list.append({'title' : title2})
 
-def goint_page(link) :
-    headers = {
-        "Referer"  : "https://m.cafe.naver.com/ArticleSearchList.nhn?search.query=%EA%B0%80%EB%A1%9C%EC%88%98%EA%B8%B8%EB%A7%9B%EC%A7%91&search.menuid=&search.searchBy=1&search.sortBy=date&search.clubid=11262350&search.option=0&search.defaultValue=1",
-        "User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36",
-    }
-    # req = urllib.request.Request(link, headers = headers)
-    # #response = requests.get(link, headers = headers)
-    # html = urllib.request.urlopen(req).read()
-    session = dryscrape.Session()
-    session.visit(link)
-    response = session.body()
-    soup = BeautifulSoup(html, "html.parser")
-    text = soup.select_one("#postContent").text
-    return text
-#hangul = hangul.decode("가로수길").encode('utf-8')
-# s = "가로수길"
-# s3 = s.encode('cp949')
-# s3 = str(s3)
-#print(s.encode('utf-8'))
-check = get_list()
-ls = get_link(check)
-text = goint_page(ls[0])
-
-print(text)
+print(title_list)
+cafe_df = pd.DataFrame(title_list)
+cafe_df.to_csv('cafe_crawling.csv', index=False)
+# keyword = "가로수길맛집"
+# driver.find_element_by_xpath('//*[id="topLayerQueryInput"]').
